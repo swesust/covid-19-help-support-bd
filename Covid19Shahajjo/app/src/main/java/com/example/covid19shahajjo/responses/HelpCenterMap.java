ea128efecdd077f7de2c65bea0e98fa34dffe049
mhRumi <rumiswe@gmail.com>
2020-04-16 20:55:16 +0600

    Now map focus on user's current location

--- Covid19Shahajjo/app/src/main/java/com/example/covid19shahajjo/responses/HelpCenterMap.java
+++ Covid19Shahajjo/app/src/main/java/com/example/covid19shahajjo/responses/HelpCenterMap.java
@@ -1,12 +1,23 @@
 package com.example.covid19shahajjo.responses;
 
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 
 import com.example.covid19shahajjo.R;
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.location.LocationEngineRequest;
 import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -19,14 +30,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.List;
 
 
-/**
- * The most basic example of adding a map to an activity.
- */
 public class HelpCenterMap extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
 
     private MapView mapView;
     public static MapboxMap mapboxMap;
     private BuildingPlugin buildingPlugin;
+    public static Context context;
+    private PermissionsManager permissionsManager;
+
+    public static final long DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L;
+    public static final long DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +48,7 @@ public class HelpCenterMap extends AppCompatActivity implements OnMapReadyCallba
         Mapbox.getInstance(this, getString(R.string.access_token));
 
         setContentView(R.layout.activity_help_center_map);
+        context = getApplicationContext();
 
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
@@ -50,10 +64,32 @@ public class HelpCenterMap extends AppCompatActivity implements OnMapReadyCallba
                 buildingPlugin = new BuildingPlugin(mapView, mapboxMap, style);
                 buildingPlugin.setMinZoomLevel(15f);
                 buildingPlugin.setVisibility(true);
+                enableLocationComponent(style);
 
             }
         });
     }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+
+            locationComponent.setLocationComponentEnabled(true);
+
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
 
     // Add the mapView lifecycle to the activity's lifecycle methods
     @Override
@@ -105,6 +141,17 @@ public class HelpCenterMap extends AppCompatActivity implements OnMapReadyCallba
 
     @Override
     public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(this, "user_location_permission_not_granted", Toast.LENGTH_LONG).show();
+            finish();
+        }
 
     }
 
