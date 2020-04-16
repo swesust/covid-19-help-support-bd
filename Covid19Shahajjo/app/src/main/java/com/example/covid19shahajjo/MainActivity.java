98ebb0b2000fb8257a1da661aa7c03b3691c740f
Abdullahfoysal <abdullah32@student.sust.edu>
2020-04-16 23:20:51 +0600

    phoneCall Service can get from any Activity(Instruction is given on that class)

--- Covid19Shahajjo/app/src/main/java/com/example/covid19shahajjo/MainActivity.java
+++ Covid19Shahajjo/app/src/main/java/com/example/covid19shahajjo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.covid19shahajjo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
