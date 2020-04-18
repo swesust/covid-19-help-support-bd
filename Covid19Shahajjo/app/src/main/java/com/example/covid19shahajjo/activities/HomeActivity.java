dc7fa7ad73f9d27a3859bd999605bf27076bfddd
Rafiul Islam <avoidcloud@gmail.com>
2020-04-18 15:14:41 +0600

    Home menus and actions

--- Covid19Shahajjo/app/src/main/java/com/example/covid19shahajjo/activities/HomeActivity.java
+++ Covid19Shahajjo/app/src/main/java/com/example/covid19shahajjo/activities/HomeActivity.java
@@ -2,15 +2,68 @@ package com.example.covid19shahajjo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.covid19shahajjo.R;
+import com.example.covid19shahajjo.adapters.HomeMenuAdapter;
+import com.example.covid19shahajjo.utils.Enums;
+import com.example.covid19shahajjo.utils.SharedStorge;
 
-public class HomeActivity extends AppCompatActivity {
+
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView menuView;
+    private Enums.Language userLang;
+    private HomeMenuAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setTitle("Home");
+        checkPreconditions();
+        layoutComponentMapping();
+    }
+
+    private void checkPreconditions(){
+        Enums.Language language =  SharedStorge.getUserLanguage(this);
+        if(language == Enums.Language.NONE){
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        }
+        else{
+            language = language;
+        }
+    }
+
+    private void layoutComponentMapping(){
+        menuView = (ListView) findViewById(R.id.home_menu_list);
+        String[] menus = userPreferableMenu();
+        adapter = new HomeMenuAdapter(this, R.layout.layout_home_menu_item, menus);
+        menuView.setAdapter(adapter);
+        menuView.setOnItemClickListener(this);
+    }
+
+    private String[] userPreferableMenu(){
+        String menus[];
+        if(userLang == Enums.Language.EN){
+            menus = getResources().getStringArray(R.array.menu_en);
+        }else{
+            menus = getResources().getStringArray(R.array.menu_bd);
+        }
+       return menus;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        intentOnClickAction(position);
+    }
+
+    private void intentOnClickAction(int position){
+
     }
 }
---
+++ Covid19Shahajjo/app/src/main/java/com/example/covid19shahajjo/adapters/HomeMenuAdapter.java
@@ -0,0 +1,69 @@
+package com.example.covid19shahajjo.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.example.covid19shahajjo.R;
+
+
+public class HomeMenuAdapter extends ArrayAdapter<String> {
+
+    private Context context;
+    private int resource;
+    private String menus[];
+    private ImageView menuIcon;
+    private TextView menuText;
+
+    public HomeMenuAdapter(@NonNull Context context, int resource, String[] menus) {
+        super(context, resource, menus);
+        this.context = context;
+        this.resource = resource;
+        this.menus = menus;
+    }
+
+    @Override
+    public int getCount(){
+        return this.menus.length;
+    }
+
+    @Override
+    public String getItem(int index){
+        return this.menus[index];
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View row = convertView;
+        if(row == null){
+            LayoutInflater inflater = getLayoutInflater();
+            row = inflater.inflate(this.resource, parent, false);
+        }
+
+        String menu = getItem(position);
+        menuIcon = (ImageView) row.findViewById(R.id.home_menu_item_icon);
+        menuText = (TextView) row.findViewById(R.id.home_menu_item_text);
+        menuIcon.setImageResource(getDrawable(position));
+        menuText.setText(menu);
+        return row;
+    }
+
+    private LayoutInflater getLayoutInflater(){
+        return (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    private int getDrawable(int position){
+        switch (position){
+            case 0: return R.drawable.ic_help_contact;
+            case 1: return R.drawable.ic_hospital;
+            case 2: return R.drawable.ic_statistics;
+            default: return -1;
+        }
+    }
+}
